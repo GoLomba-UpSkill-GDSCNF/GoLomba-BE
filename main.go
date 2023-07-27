7554dc2c688d434426bdd95164ac86c3dec91fdd
notRaihan <114415754+notRaihan@users.noreply.github.com>
2023-07-27 21:25:52 +0700

    auto migrate models pkg using GORM

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/notRaihan/GoLomba-BE-GDSC-Final-Project/platform/database"
 )
 
 func main() {
+	database.Connect()
 	app := fiber.New()
 
 	app.Get("/api", func(c *fiber.Ctx) error {
--- platform/database/mysql.go
+++ platform/database/mysql.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/notRaihan/GoLomba-BE-GDSC-Final-Project/app/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -16,7 +17,7 @@ type Instance struct {
 var DB Instance
 
 func Connect() {
-	dsn := "root:root@tcp(localhost:3306)/golomba_db?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root@tcp(localhost:3306)/golomba_db?charset=utf8mb4&parseTime=True&loc=Local"
 	dbConnection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
@@ -26,9 +27,10 @@ func Connect() {
 
 	log.Println("Database connected!")
 	dbConnection.Logger = logger.Default.LogMode(logger.Info)
-
 	log.Println("Running migrations...")
-	// TODO: Add migrations
+
+	// Add migrations
+	dbConnection.AutoMigrate(&models.User{}, &models.Role{}, &models.Tag{}, &models.Competition{}, &models.EducationLevel{}, &models.Testimonial{})
 
 	DB = Instance{Db: dbConnection}
 }
