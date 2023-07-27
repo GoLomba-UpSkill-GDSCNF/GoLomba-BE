0bff2c252deb8ff52afd95a7a18b9e8357a97386
notRaihan <114415754+notRaihan@users.noreply.github.com>
2023-07-27 11:46:37 +0700

    feat: Install gofiber, gorm, gorm driver mysql, and air live reload
    This commit installs the gofiber, gorm,  gorm driver mysql, and air live reload packages to the project.

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/api", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
