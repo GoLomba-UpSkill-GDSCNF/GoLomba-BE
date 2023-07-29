db2b0763d3626cd083649f7abb5f1482aa4c1fec
notRaihan <114415754+notRaihan@users.noreply.github.com>
2023-07-29 22:28:12 +0700

    refactor: separate routes

--- main.go
+++ main.go
@@ -4,26 +4,17 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/notRaihan/GoLomba-BE-GDSC-Final-Project/app/controllers"
-	"github.com/notRaihan/GoLomba-BE-GDSC-Final-Project/app/models"
-	"github.com/notRaihan/GoLomba-BE-GDSC-Final-Project/pkg/middleware"
+	"github.com/notRaihan/GoLomba-BE-GDSC-Final-Project/pkg/routes"
 	"github.com/notRaihan/GoLomba-BE-GDSC-Final-Project/platform/database"
 )
-
-func SetupTagRoutes(app *fiber.App) {
-	app.Post("/tag", middleware.ValidateField[models.Tag](), controllers.CreateTag)
-	app.Get("/tags", controllers.GetTags)
-	app.Get("/tag/:id", controllers.GetTag)
-	app.Put("/tag/:id", middleware.ValidateField[models.Tag](), controllers.UpdateTag)
-	app.Delete("/tag/:id", controllers.DeleteTag)
-}
 
 func main() {
 	database.Connect()
 
 	app := fiber.New()
 
-	SetupTagRoutes(app)
+	routes.SetupTagRoutes(app)
+	routes.SetupEducationLevelRoutes(app)
 
 	app.Get("/api", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
